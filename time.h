d19397a2309e8cbd64e6ba180046ef71af4daf76
khademul <khademul144@gmail.com>
2015-03-19 21:47:11 -0500

    changed name to lowercase

---
+++ time.h
@@ -0,0 +1,50 @@
+/*
+ * Time.h
+ * Header file
+ * khademul haque
+ */
+
+#ifndef TIME_H_
+#define TIME_H_
+
+struct Time {
+ private:
+  int hour;
+  int minute;
+  int second;
+  int milisecond;
+ public:
+  int getHour() const {
+	return hour;
+ }
+
+ void setHour(int hour) {
+	this->hour = hour;
+ }
+
+ int getMilisecond() const {
+	return milisecond;
+ }
+
+ void setMilisecond(int milisecond) {
+	this->milisecond = milisecond;
+ }
+
+ int getMinute() const {
+	return minute;
+ }
+
+ void setMinute(int minute) {
+	this->minute = minute;
+ }
+
+ int getSecond() const {
+	return second;
+ }
+
+ void setSecond(int second) {
+	this->second = second;
+ }
+};
+
+#endif /* TIME_H_ */
